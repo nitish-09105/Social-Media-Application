79cbcfab0e521404cba64671639ffa9b98ec7e18
NITISH KUMAR <81707719+nitish-09105@users.noreply.github.com>
2024-01-13 23:30:11 +0530

    Frontend

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home/> */}
+      {/* <Profile/> */}
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
